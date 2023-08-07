88409540ef2189d6c1ac45fba1c6269958ca6bc3
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-08-07 19:55:52 -0300

    Update unix-driver.sh

--- unix-driver.sh
+++ unix-driver.sh
@@ -141,7 +141,7 @@ function tpmDriver() {
 
 # Install Plugins
 function pluginsDriver() {
-  curl -o /home/$(whoami)/.tmux.conf https://raw.githubusercontent.com/GabrielFlores8227/Tmux-Config/master/.tmux.conf && tmux source ~/.tmux.conf && ~/.tmux/plugins/tpm/bin/install_plugins
+  curl -o /home/$(whoami)/.tmux.conf https://raw.githubusercontent.com/GabrielFlores8227/Tmux-Config/master/.tmux.conf && tmux source ~/.tmux.conf #&& ~/.tmux/plugins/tpm/bin/install_plugins
 
 function executeDriver() {
       echo -e "\n\n\033[0;37;43m[*] Installing $1\033[m" \
